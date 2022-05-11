21d17906ae3e8b749c3701e59b8bcacc4047f0d8
ViktorPavukNULP <viktor.pavuk.kn.2017@lpnu.ua>
2022-05-11 15:01:20 +0300

    users

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IUser} from "../models/IUser";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url = 'http://jsonplaceholder.typicode.com/users';
+
+  constructor(private http:HttpClient) {
+  }
+
+  getUsers(): Observable<IUser[]>{
+    return this.http
+      .get<any[]>(this.url)
+  }
+
+}
