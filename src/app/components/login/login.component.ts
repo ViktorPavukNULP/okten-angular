2c44d6df8525e59a6c1673f96cf9fbbe1c9209c2
ViktorPavukNULP <viktor.pavuk.kn.2017@lpnu.ua>
2022-05-30 15:12:57 +0300

    homework5 login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {
+    this._createForm()
+  }
 
   ngOnInit(): void {
+  }
+  _createForm(): void {
+    this.form = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  login(): void {
+    this.authService.login(this.form.value).subscribe(value => {
+      this.authService.setToken(value);
+      this.router.navigate(['cars']);
+    });
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,15 +4,38 @@ import {HttpClient} from "@angular/common/http";
 import {urls} from "../constants/urls";
 import {IUser} from "../interfaces/user.interface";
 import {Observable} from "rxjs";
+import {IToken} from "../interfaces/token.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private accessTokenKey = 'access';
 
   constructor(private httpClient: HttpClient) {
   }
   register(user:IUser): Observable<IUser> {
     return this.httpClient.post<IUser>(urls.users, user);
   }
+
+  login(user:IUser): Observable<IToken> {
+    return this.httpClient.post<IToken>(urls.auth, user);
+  }
+
+  setToken(token: IToken): void {
+    localStorage.setItem(this.accessTokenKey, token.access);
+  }
+
+  getToken(): string {
+    return localStorage.getItem(this.accessTokenKey) as string;
+  }
+
+  deleteToken(): void {
+    localStorage.removeItem(this.accessTokenKey);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
 }
